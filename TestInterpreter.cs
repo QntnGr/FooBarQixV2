7c4419ca5d30ab0e213a693fa3bc3002043937fa
fooBarQix addict <>
2023-10-23 10:59:37 +0200

    tests non exécutable ??

---
+++ TestInterpreter.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooBarQixV2
+{
+    internal class TestInterpreter
+    {
+
+        [Test]
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "FooFoo")]
+        [TestCase(4, "4")]
+        [TestCase(5, "BarBar")]
+        [TestCase(6, "Foo")]
+        [TestCase(7, "QixQix")]
+        [TestCase(8, "8")]
+        [TestCase(9, "Foo")]
+        [TestCase(10, "Bar*")]
+        [TestCase(13, "Foo")]
+        [TestCase(15, "FooBarBar")]
+        [TestCase(21, "FooQix")]
+        [TestCase(33, "FooFooFoo")]
+        [TestCase(51, "FooBar")]
+        [TestCase(53, "BarFoo")]
+        [TestCase(101, "1*1")]
+        [TestCase(303, "FooFoo*Foo")]
+        [TestCase(105, "FooBarQix*Bar")]
+        [TestCase(10101, "FooQix**")]
+        public void TestNumberConverterStageOne(int number, string expectedString)
+        {
+            var interpreter = new NumberInterpreter(number);
+            interpreter.GetExpressionByNumber();
+
+            Assert.AreEqual(expectedString, interpreter.GetOutputExpression());
+        }
+    }
+}
